abcbd32de6ed9bd109ec56f8c73b1ff25fb3f9bd
Chim01l <l.chimera01@gmail.com>
2024-09-02 15:03:16 +0300

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -3,17 +3,17 @@ window1 = Tk()
 
 def fun1():
     check1 = IntVar()
+    check1 = True
     checkbox1 = Checkbutton(window1, variable=check1, text="Что-то там")
     checkbox1.grid(row=1, column=0)
     checkbox1.select()
+    check2 = IntVar()
+    check2 = True
+    checkbox2 = Checkbutton(window1, variable=check2, text="Другой текст")
+    checkbox2.grid(row=3, column=0)
+    checkbox2.select()
 
 
 fun1()
-
-check2 = IntVar()
-checkbox2 = Checkbutton(window1, variable=check2, text="Что-то там")
-checkbox2.grid(row=3, column=0)
-checkbox2.select()
-
 
 window1.mainloop()
